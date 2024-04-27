47e23b40f4467182882e7d9060bea98a72b7ce3e
F0sya <nikitachmyria@gmail.com>
2024-04-27 16:38:06 +0300

    fixed. w/o <string>

--- if-else-cpp/main.cpp
+++ if-else-cpp/main.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main() {
 	int sum_x1 = 0;
 	int sum_x2 = 0;
-	string x; cout << "Your number:"; cin >> x;
-	if (x.length() != 6) {
+	int x; cout << "Your number:"; cin >> x;
+	if (x / 100000 < 1 || x / 100000 > 9) {
 		cout << "Your number need to be 6-digit!" << endl;
 		return 0;
 	}
-	string x1 = x.substr(0, 3);
-	string x2 = x.substr(3, 3);
-	for (char ch : x1) {
-		sum_x1 += ch - '0';
+	int x1 = x / 1000;
+	int x2 = x % 1000;
+	while (x1 > 0) {
+		int i = x1 % 10;
+		sum_x1 += i;
+		x1 /= 10;
 	}
-	for (char ch : x2) {
-		sum_x2 += ch - '0';
+	while (x2 > 0) {
+		int i = x2 % 10;
+		sum_x2 += i;
+		x2 /= 10;
 	}
 	(sum_x1 == sum_x2) ? cout << "Lucky ticket! :)" << endl : cout << "Unlucky ticket! :(" << endl;
 }
