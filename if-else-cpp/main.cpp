a3a5428ff410e49069c2b79382bbcda6616d72cb
F0sya <nikitachmyria@gmail.com>
2024-04-27 16:55:38 +0300

    fixed. w/o cycles

--- if-else-cpp/main.cpp
+++ if-else-cpp/main.cpp
@@ -2,8 +2,6 @@
 using namespace std;
 
 int main() {
-	int sum_x1 = 0;
-	int sum_x2 = 0;
 	int x; cout << "Your number:"; cin >> x;
 	if (x / 100000 < 1 || x / 100000 > 9) {
 		cout << "Your number need to be 6-digit!" << endl;
@@ -11,15 +9,11 @@ int main() {
 	}
 	int x1 = x / 1000;
 	int x2 = x % 1000;
-	while (x1 > 0) {
-		int i = x1 % 10;
-		sum_x1 += i;
-		x1 /= 10;
-	}
-	while (x2 > 0) {
-		int i = x2 % 10;
-		sum_x2 += i;
-		x2 /= 10;
-	}
+
+	int sum_x1 = (x1 % 10) + ((x1 / 10) % 10) + ((x1 / 100) % 10);
+	int sum_x2 = (x2 % 10) + ((x2 / 10) % 10) + ((x2 / 100) % 10);
+
+
+
 	(sum_x1 == sum_x2) ? cout << "Lucky ticket! :)" << endl : cout << "Unlucky ticket! :(" << endl;
 }
